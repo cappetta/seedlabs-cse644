f30a7cceed75473bcbd7b339a64b8d57356f5ab3
~Cappetta <thomas.cappetta@gmail.com>
2020-10-19 13:15:27 -0400

    working task 1.4

--- Sniffing_Spoofing/Scapy/sniff_and_spoof.py
+++ Sniffing_Spoofing/Scapy/sniff_and_spoof.py
@@ -2,10 +2,14 @@ from scapy.all import *
 
 def print_pkt(pkt):
     p = copy.deepcopy(pkt[IP])
-    p.src = pkt[IP].dst
-    #p.src = '1.1.1.1'
+#    p.src = pkt[IP].dst
+    p.src = '1.1.1.1'
     p.dst = pkt[IP].src
     p[ICMP].type = 0
+    del p[IP].chksum
+    del p[ICMP].chksum
+    #p[IP].ttl=255
+    p.show2()
     send(p)
     
 pkt = sniff(filter='icmp[icmptype]=8', prn=print_pkt)    
